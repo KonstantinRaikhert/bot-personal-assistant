9afb15492eeb69391ac156fbb57902b6964b707d
Konstantin Raikhert <raikhert13@gmail.com>
2023-04-17 00:00:56 +0300

    file handling

--- bot-assistent/config_data/config.py
+++ bot-assistent/config_data/config.py
@@ -10,45 +10,26 @@ from environs import Env
 #     db_password: str
 
 
-# @dataclass
-# class TgBot:
-#     token: str
-#     admin_ids: list[int]
-
-
-# @dataclass
-# class Config:
-#     tg_bot: TgBot
-#     db: DatabaseConfig
-
-
-# def load_config(path: str) -> Config:
-#     env: Env = Env()
-#     env.read_env(path=path)
-
-#     config = Config(
-#         tg_bot=TgBot(token=env("TELEGRAM_TOKEN"), admin_ids=env("ADMIN_IDS")),
-#         db=DatabaseConfig(
-#             database=env("DATABASE"), db_host=env("DB_HOST"), db_user=env("DB_USER"), db_password=env("DB_PASSWORD")
-#         ),
-#     )
-#     return config
-
-
 @dataclass
 class TgBot:
     token: str
+    admin_ids: list[int]
 
 
 @dataclass
 class Config:
     tg_bot: TgBot
+    # db: DatabaseConfig
 
 
 def load_config(path: str) -> Config:
     env: Env = Env()
     env.read_env(path=path)
 
-    config = Config(tg_bot=TgBot(token=env("TELEGRAM_TOKEN")))
-
+    config = Config(
+        tg_bot=TgBot(token=env("TELEGRAM_TOKEN"), admin_ids=list(map(int, env.list("ADMIN_IDS")))),
+        # db=DatabaseConfig(
+        #     database=env("DATABASE"), db_host=env("DB_HOST"), db_user=env("DB_USER"), db_password=env("DB_PASSWORD")
+        # ),
+    )
     return config
---
+++ bot-assistent/services/file_handling.py
@@ -0,0 +1,47 @@
+from math import ceil
+
+BOOK_PATH = "bot-assistent/book/book.txt"
+PAGE_SIZE = 1050
+
+
+book: dict[int, str] = {}
+
+
+def _get_part_text(text: str, start: int, size: int) -> tuple[str, int]:
+    end = start + size
+    prep_list = [",", ".", "!", ":", ":", "?"]
+    if len(text) < end:
+        new_text = text[start:]
+        return new_text, len(new_text)
+    if text[end] in prep_list:
+        end -= 1
+    while text[end] not in prep_list:
+        end -= 1
+    if text[end + 1] != ".":
+        new_end = end + 1
+        new_text = text[start:new_end]
+        return new_text, len(new_text)
+    else:
+        if text[end - 1] in prep_list:
+            end -= 2
+        else:
+            end -= 1
+        while text[end] not in prep_list:
+            end -= 1
+        new_end = end + 1
+        new_text = text[start:new_end]
+        return new_text, len(new_text)
+
+
+def prepare_book(path: str) -> None:
+    with open(path, "r") as file:
+        text_str = "".join(file.readlines())
+        count = ceil(len(text_str) / PAGE_SIZE)
+        start = 0
+        for i in range(1, count + 1):
+            text_line = _get_part_text(text=text_str, start=start, size=PAGE_SIZE)[0]
+            book[i] = text_line.lstrip("\n ")
+            start += len(text_line)
+
+
+prepare_book(BOOK_PATH)
